d75dbb5651d9073f127d8cc48b71407da52889d4
jcamilofarfan <farfan1976@hotmail.com>
2025-05-20 15:10:25 -0500

    Fix installation script to correct git clone command and directory navigation

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd "$HOME"
-git clone git clone https://github.com/slice-soft/ss-bootstrap-python-iot app
-cd app
+git clone git clone https://github.com/slice-soft/ss-bootstrap-python-iot
+cd ss-bootstrap-python-iot
 chmod +x *.sh
 ./run.sh
 (crontab -l 2>/dev/null; echo "*/5 * * * * $HOME/app/updater.sh >> $HOME/app/update.log 2>&1") | crontab -
