de4aaf277e2277679146f9c71d48b99cea90f3e6
jcamilofarfan <farfan1976@hotmail.com>
2025-05-20 14:34:40 -0500

    Fix installation script to use $HOME variable for user directory

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ~
+cd "$HOME"
 git clone https://github.com/slice-soft/raspi-vehiculo-bootstrap app
 cd app
 chmod +x *.sh
 ./run.sh
-(crontab -l 2>/dev/null; echo "*/5 * * * * /home/pi/app/updater.sh >> /home/pi/app/update.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * $HOME/app/updater.sh >> $HOME/app/update.log 2>&1") | crontab -
