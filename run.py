9b0595893f8ae08c65197e4e53f438ef50b3857f
jcamilofarfan <farfan1976@hotmail.com>
2025-05-20 18:55:30 -0500

    Update log message in run.py to reflect version 1.1.3

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ log_path = os.path.join(os.environ["HOME"], "ss-bootstrap-python-iot", "log.txt"
 
 while True:
     with open(log_path, "a") as f:
-        f.write(f"[{datetime.now()}] ✅ Sistema en ejecución V1.1.2\n")
+        f.write(f"[{datetime.now()}] ✅ Sistema en ejecución V1.1.3\n")
     time.sleep(60)
