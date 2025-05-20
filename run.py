00150226657a527f07eb79869c55ac234e0e8b3d
jcamilofarfan <farfan1976@hotmail.com>
2025-05-20 18:16:04 -0500

    Update log message in run.py to include version number

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ log_path = os.path.join(os.environ["HOME"], "ss-bootstrap-python-iot", "log.txt"
 
 while True:
     with open(log_path, "a") as f:
-        f.write(f"[{datetime.now()}] ✅ Sistema en ejecución\n")
+        f.write(f"[{datetime.now()}] ✅ Sistema en ejecución V1.0\n")
     time.sleep(60)
