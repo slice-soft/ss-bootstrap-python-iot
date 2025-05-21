669b3a036c14f1ad0978be6d71682f863f81239f
jcamilofarfan <farfan1976@hotmail.com>
2025-05-21 08:31:21 -0500

    Update log message in run.py to use VERSION variable

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import time
 import os
+VERSION = "V1.2.0"
 
 log_path = os.path.join(os.environ["HOME"], "ss-bootstrap-python-iot", "log.txt")
 
 while True:
     with open(log_path, "a") as f:
-        f.write(f"[{datetime.now()}] ✅ Sistema en ejecución V1.1.3\n")
+        f.write(f"[{datetime.now()}] ✅ Sistema en ejecución {VERSION}\n")
     time.sleep(60)
