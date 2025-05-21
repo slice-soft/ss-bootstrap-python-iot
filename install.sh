37c81cbee0b29ddd445f0d8f470e4b4aa509115b
jcamilofarfan <farfan1976@hotmail.com>
2025-05-21 08:34:01 -0500

    Refactor install.sh to define repository URL and installation directory, and clone only if the directory does not exist

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-cd "$HOME"
-git clone https://github.com/slice-soft/ss-bootstrap-python-iot
-cd ss-bootstrap-python-iot
-chmod +x *.sh
+REPO_URL="https://github.com/slice-soft/ss-bootstrap-python-iot"
+INSTALL_DIR="$HOME/ss-bootstrap-python-iot"
+
+# Clonar el repositorio solo si no existe
+if [ ! -d "$INSTALL_DIR" ]; then
+  cd "$HOME"
+  git clone "$REPO_URL"
+else
+  echo "📁 El repositorio ya existe en $INSTALL_DIR. Se omitió la clonación."
+fi
+
+find "$INSTALL_DIR" -maxdepth 1 -name "*.sh" -exec chmod +x {} \;
 ./run.sh
 (crontab -l 2>/dev/null; echo "*/5 * * * * $HOME/ss-bootstrap-python-iot/updater.sh >> $HOME/ss-bootstrap-python-iot/update.log 2>&1") | crontab -
